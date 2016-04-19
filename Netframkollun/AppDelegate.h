0b520e3774e65e03ed25b382dfa2d4dd0d777f49
athorhallsson <athorhallsson@gmail.com>
2016-04-19 17:04:35 +0000

    Initial Commit

---
+++ Netframkollun/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Netframkollun
+//
+//  Created by Andri Thorhallsson on 19/04/16.
+//  Copyright © 2016 Andri Thorhallsson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Netframkollun/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Netframkollun
+//
+//  Created by Andri Thorhallsson on 19/04/16.
+//  Copyright © 2016 Andri Thorhallsson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
